7af7515ff0b022cb322995aaf7401848176c5651
Anjali28-ish <anjalikumarimishra28@gmail.com>
2025-12-14 17:47:15 +0530

    Changes in serverjs

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,47 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
+const dotenv =  require("dotenv");
 const connectDB = require("./config/db");
-const userRoutes = require("./Routes/userRoutes");
-const productsRoutes = require("./Routes/productsRoutes");
-const cartRoutes = require("./Routes/cartRoutes");
-const checkoutRoutes = require("./Routes/CheckoutRoutes");
-const orderRoutes = require("./Routes/orderRoutes");
-const uploadRoutes = require("./Routes/uploadRoutes");
-const subscribeRoute = require("./Routes/subscribeRoute");
-const adminRoutes = require("./Routes/adminRoutes");
-const productAdminRoutes = require("./Routes/productAdminRoutes");
-const adminOrderRoutes = require("./Routes/adminOrderRoutes");
-
-dotenv.config();
-
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productsRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const checkoutRoutes = require("./routes/checkoutRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
+const subscribeRoute = require("./routes/subscribeRoute");
+const adminRoutes = require("./routes/adminRoutes");
+const productAdminRoutes = require("./routes/productAdminRoutes");
+const adminOrderRoutes = require("./routes/adminOrderRoutes");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-// ✅ CORS setup (handles preflight automatically)
-app.use(cors({
-origin: "http://localhost:5173",
-methods: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
-credentials: true,
-}));
+dotenv.config();  
+
 
 const PORT = process.env.PORT || 3000;
 
+// Connect to MongoDB
 connectDB();
 
 app.get("/", (req, res) => {
-res.send("WELCOME TO RABBIT API!");
+  res.send("WELCOME TO RABBIT API!");
 });
 
 // API Routes
 app.use("/api/users", userRoutes);
-app.use("/api/products", productsRoutes);
+app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
-app.use("/api", subscribeRoute);
+app.use("/api/", subscribeRoute);
 
-// Admin Routes
+// Admin
 app.use("/api/admin/users", adminRoutes);
 app.use("/api/admin/products", productAdminRoutes);
 app.use("/api/admin/orders", adminOrderRoutes);
 
 app.listen(PORT, () => {
-console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
